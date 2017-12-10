9421488401bc8abda22affbb4ea1b85d5f8319d8
tcolen6 <tcolen6@gmail.com>
2017-12-09 23:36:40 -0500

    Create question.php

---
+++ question.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Question <?php echo $question_number; ?></title>
+</head>
+<body bgcolor="#4286f4" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+	<table width="15%">
+		<tr><td><FONT color = "#FFFFFF">Logged in as: <?php echo $user; ?></FONT></td></tr>
+		<tr><td><a color = "#FFFFFF" href="main.php?action=signout">Sign Out</a></td></tr>
+	</table>
+	<table width="30%" border="0" cellspacing="5px" cellpadding="10px" align="center" height="50%">
+		<tr>
+			<td><FONT color = "#FFFFFF"><STRONG>Question <?php echo $question_number; ?></STRONG></FONT></td>
+		</tr>
+		<tr>
+			<td><FONT color = "#FFFFFF"><?php echo $question;?></FONT>
+		</tr>
+		<form method="post" <?php echo 'action="main.php?action=next&user='.$user.'&question='.$question_number.'"';?>>
+		<?php
+			for ($i=0; $i < sizeof($answers); $i++) { 
+				echo '<tr><td><input type="radio" name="answer" value="'.$answers[$i].'"><FONT color = "#FFFFFF">'.$answers[$i].'</FONT></td></tr>';
+			}
+		?>
+			<tr>
+				<td>
+					<input type="submit" name="submit" value="Next Question">
+				</td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
